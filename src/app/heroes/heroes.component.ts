4291e6bdd6e59b40bdabb18ef33384bf8debb4e0
ogaliz <oscargalvezr@live.com>
2019-09-26 19:18:19 +0200

    Mostrando en la vista propiedades de una clase

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
 
+// Estilos y vistas que pertenecen al componente
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
+  hero : Hero = {
+    id : 1,
+    name : 'Oscar Galvez'
+  };
 
   constructor() { }
 
